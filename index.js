4af156309f94e49b31b1ee2e6ab614fd6209894c
steephen varghese <steephen.varghese@cubettech.com>
2020-05-23 11:17:26 +0530

    db url connected and made project runnable

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require("express");
+const mongoose = require('mongoose');
 var bodyParser = require("body-parser");
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/auth-test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 var userController = require("./controllers/User")
 
@@ -30,12 +36,16 @@ app.get("/signup", function (req, res) {
 app.post("/login", async function (req, res) {
   const user = new userController();
   const token = await user.login(req.body.email, req.body.password);
-  return res.json({token});
+  return res.json({
+    token
+  });
 });
 app.post("/signup", async function (req, res) {
   const user = new userController();
   const token = await user.signup(req.body);
-  return res.json({success:true});
+  return res.json({
+    success: true
+  });
 });
 
 
